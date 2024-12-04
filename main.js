b3d8f56f08057b50d9325614dbbbf31eb35469be
adityagoswami327 <adityagoswami327@gmail.com>
2024-12-04 14:01:46 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Add JS here
+let menuIcon = document.querySelector(".menu-icon");
+let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+
+// let menuIcon = document.querySelector(".menu-icon")
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");    
+    
+}
